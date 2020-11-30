f86a48ea928c569ef8e65536dd165a5025e956cc
Sarah Power <sarahjonipower0@gmail.com>
2020-11-30 15:43:27 +0000

    top_up method added

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,15 @@ describe Oystercard do
   it 'has a starting balance of zero' do
     expect(subject.balance).to eq 0
   end
+
+  describe '#top_up' do
+
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'should add amount to balance' do
+      subject.top_up(5)
+      expect(subject.balance).to eq 5
+    end
+  end
+
 end
