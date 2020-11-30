8fad30d2bffed9fb2b3c3b0498c09d3417ac95c1
Lisa <bardelli.lisa@gmail.com>
2020-11-30 17:10:06 +0000

    Check minimum balance on touch in

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,28 +1,30 @@
-
 class Oystercard
-MAXIMUM_BALANCE = 90
-attr_reader :balance , :in_journey
+  MAXIMUM_BALANCE = 90
+  MINIMUM_BALANCE = 1
+  attr_reader :balance, :in_journey
 
-def initialize(balance=0)
-  @balance = balance
-  @in_journey = false
-end
+  def initialize(balance = 0)
+    @balance = balance
+    @in_journey = false
+  end
 
-def top_up(money)
-    fail "Cannot exeed max balance (#{MAXIMUM_BALANCE})!" if (@balance + money) > MAXIMUM_BALANCE
+  def top_up(money)
+    raise "Cannot exeed max balance (#{MAXIMUM_BALANCE})!" if (@balance + money) > MAXIMUM_BALANCE
+
     @balance += money
-end
+  end
 
-def deduct(money)
-    @balance -=money
-end
+  def deduct(money)
+    @balance -= money
+  end
 
-def touch_in
-  @in_journey = true
-end
+  def touch_in
+    raise 'The balance is less than (#{MINIMUM_BALANCE}). Top_up your card!' if balance < MINIMUM_BALANCE
 
-def touch_out
-  @in_journey = false
-end
+    @in_journey = true
+  end
 
+  def touch_out
+    @in_journey = false
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,42 +1,47 @@
 require 'rspec'
-require './lib/oystercard.rb'
+require './lib/oystercard'
 
 describe Oystercard do
-    let(:card){Oystercard.new}
-  it "is created with a default balance of zero" do
+  let(:card) { Oystercard.new }
+  it 'is created with a default balance of zero' do
     expect(card.balance).to eq(0)
   end
 
-  describe "#top_up" do
-    it "returns updated balance after calling top_up method" do
-        card.top_up(20)
-        expect(card.balance).to eq(20)
+  describe '#top_up' do
+    it 'returns updated balance after calling top_up method' do
+      card.top_up(20)
+      expect(card.balance).to eq(20)
     end
-    it "returns an error if top-up will exceed MAXIMUM_BALANCE" do
-      expect {card.top_up(Oystercard::MAXIMUM_BALANCE + 1)}.to raise_error("Cannot exeed max balance (#{Oystercard::MAXIMUM_BALANCE})!")
-    end
-  end
-  describe "#deduct" do
-    it "deducts amount of money from balance" do
-        card.top_up(20)
-        card.deduct(5)
-        expect(card.balance).to eq(15)
+    it 'returns an error if top-up will exceed MAXIMUM_BALANCE' do
+      expect { card.top_up(Oystercard::MAXIMUM_BALANCE + 1) }.to raise_error("Cannot exeed max balance (#{Oystercard::MAXIMUM_BALANCE})!")
     end
   end
-  describe "#touch_in" do
+  describe '#deduct' do
+    it 'deducts amount of money from balance' do
+      card.top_up(20)
+      card.deduct(5)
+      expect(card.balance).to eq(15)
+    end
+  end
+  describe '#touch_in' do
     it "changes the state of the card to 'in_journey' " do
+      card.top_up(Oystercard::MINIMUM_BALANCE + 1)
       card.touch_in
       expect(card.in_journey).to eq(true)
     end
+    it 'returns an error when the balance is less than #{MINIMUM_BALANCE}' do
+      expect { card.touch_in }.to raise_error('The balance is less than (#{MINIMUM_BALANCE}). Top_up your card!')
+    end
   end
-  describe "#touch_out" do
+  describe '#touch_out' do
     it "changes the state of the card to 'not_in_journey'" do
       card.touch_out
       expect(card.in_journey).to eq(false)
     end
   end
-  describe "#in_journey?" do
+  describe '#in_journey?' do
     it "returns true for an 'in' card" do
+      card.top_up(Oystercard::MINIMUM_BALANCE + 1)
       card.touch_in
       expect(card.in_journey).to be(true)
     end
