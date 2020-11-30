85276ae95d41a11b2dc44ea10c859c13d743c69e
Kiki Dawson <kikiadawson@gmail.com>
2020-11-30 15:15:53 +0000

    Add the balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+
+  it { is_expected.to respond_to(:balance) }
+
+  it 'has a starting balance of zero' do
+    expect(subject.balance).to eq 0
+  end
 end
