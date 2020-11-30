aca946dd85ddd03cc97f67e45105f8e2a923830a
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2020-11-30 15:11:06 +0000

    oystercards created with default balance of zero

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
-require 'oystercard'
+require 'rspec'
+require './lib/oystercard.rb'
+
 describe Oystercard do
+
+  it "is created with a default balance of zero" do
+    expect(subject.balance).to eq(0)
+  end
+  
 end
