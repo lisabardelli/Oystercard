b30e45306274c22ed4b531a03a44e302c868fc56
Irina <igiosanu29@gmail.com>
2020-11-30 21:57:32 +0000

    Top up fails if it exceeds max balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,14 @@ require "oystercard/version"
 
 module Oystercard
   class Card
+    MAX_BALANCE = 100
 
     def initialize
       @balance = 0
     end
 
     def top_up(money)
+      fail "Exceeds max balance" if balance + money >= MAX_BALANCE
       @balance = @balance + money
     end
 
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe Oystercard::Card do
       subject.top_up(10)
       expect(subject.balance).to eql(10)
     end
+
+    it "raises an error when reaches max balance" do
+      expect { subject.top_up(101) }.to raise_error "Exceeds max balance"
+    end
   end
 end
