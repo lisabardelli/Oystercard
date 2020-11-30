6fdb7d8d7f661fb92bfa0ecbbc160073b74ba3db
Will Dixon <dixon.will1@gmail.com>
2020-11-30 15:21:27 +0000

    top up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,4 +9,15 @@ describe Oystercard do
   it 'initializes with a balance of zero' do
     expect(subject.balance).to eq 0
   end
+ 
+  it 'will top up the balance' do 
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+  describe '#top_up' do
+    it 'will top up the balance' do
+      expect { subject.top_up(1) }.to change { subject.balance }.by 1 
+    
+    end
+  end
 end
