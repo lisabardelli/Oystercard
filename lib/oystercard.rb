218064deba99d31cb07ba833749985729f3d6302
Irina <igiosanu29@gmail.com>
2020-12-01 22:22:07 +0000

    added touch_in, touch_out and in_journey?

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,15 +3,28 @@ require "oystercard/version"
 module Oystercard
   class Card
     MAX_BALANCE = 100
-    attr_reader :balance
+    attr_reader :balance, :in_use
 
     def initialize
       @balance = 0
+      @in_use = false
     end
 
     def top_up(money)
       fail "Exceeds max balance" if balance + money >= MAX_BALANCE
       @balance = @balance + money
+    end
+
+    def touch_in
+      @in_use = true
+    end
+
+    def in_journey?
+      @in_use
+    end
+
+    def touch_out
+      @in_use = false
     end
 
     def deduct_fare(money)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,38 @@
-RSpec.describe Oystercard do
-  it "has a version number" do
-    expect(Oystercard::VERSION).not_to be nil
+RSpec.describe Oystercard::Card do
+
+  describe "#top_up" do
+    it "tops up the balance" do
+      subject.top_up(10)
+      expect(subject.balance).to eql(10)
+    end
+
+    it "raises an error when reaches max balance" do
+      expect { subject.top_up(101) }.to raise_error "Exceeds max balance"
+    end
+  end
+
+  describe "#deduct_fare" do
+    it "deducts the journey fare" do
+      subject.top_up(10)
+      expect { subject.deduct_fare(5) }.to change { subject.balance }.from(10).to(5)
+    end
+  end
+
+  describe "#touch_in" do
+    it "touch in to start a journey" do
+      subject.top_up(10)
+      expect(subject).not_to be_in_journey
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+  end
+
+  describe "#touch_out" do
+    it "touch out to end journey" do
+      subject.top_up(10)
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
   end
 end
