12250f6cea1862662d1a58a26916d1292f54fb23
Will Dixon <dixon.will1@gmail.com>
2020-12-01 14:02:33 +0000

    with comments

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,49 +1,50 @@
 require_relative 'station'
 
-class Oystercard
-  attr_accessor :balance, :capacity, :in_use, :minimum_fare, :start_station
+class Oystercard # class (Object)
+  attr_accessor :balance, :capacity, :in_use, :minimum_fare, :start_station  # when we need to change variables attr accessor allows us both read and write instance variables without using def name @name end (reader) and def name=(str) @name = str end (writer)
 
-  DEFAULT_CAPACITY = 90
+
+  DEFAULT_CAPACITY = 90 # constant is like a variable except its value is supposed to remain constant for the duration of the program (allows it to be changed for different users i.e different capacity/different prices etc)
   MINIMUM_FARE = 1
 
-  def initialize
-    @balance = 0
-    @capacity = DEFAULT_CAPACITY
-    @in_use = false
-    @minimum_fare = MINIMUM_FARE
+  def initialize  # this allows the class to know what each of the below methods gives us
+    @balance = 0 # sets starting balance to 0 
+    @capacity = DEFAULT_CAPACITY #sets capacity to the value of the DEFAULT_CAPACITY constant
+    @in_use = false # sets the starting status of in_use as false as it hasnt been used yet
+    @minimum_fare = MINIMUM_FARE #sets the minimum_fare to the value of the MINIMUM_FARE constant
   end
 
-  def top_up(money)
-    fail "Cap  of #{@capacity} reached" if (@balance + money) > @capacity
-    @balance += money
+  def top_up(money) #method (property of owner)
+    fail "Cap  of #{@capacity} reached" if (@balance + money) > @capacity # fail if balance + money is more than the capacity of the oystercard
+    @balance += money # same as balance = balance + money
   end
 
-  def deduct(money)
-    @balance -= money
+  def deduct(money) # method (property of owner) with the argument of money as you want call money on deduct
+    @balance -= money # same as balance = balance - money
+  end
+ 
+   def touch_in(station) #method with the argument of station 
+    fail "Not enough funds" if @balance < 1 # fail if balance is below one (as it is lower than minimum fair)
+    @in_use = true # sets in_use to be true
+    @start_station = station #start station is equal to the station we touch_in at 
   end
 
-  def touch_in(station)
-    fail "Not enough funds" if @balance < 1
-    @in_use = true
-    @start_station = station
+  def touch_out #method of touch_out
+    deduct(@minimum_fare) #deducts the minimum fare from the balance when touch_out is called
+    @in_use = false # once touched out the status of in_use turns to false
   end
 
-  def touch_out
-    deduct(@minimum_fare)
-    @in_use = false
+  def in_journey? #method of in_journey
+    @in_use == true # if in_use is equal to true then oystercard is in journey
   end
 
-  def in_journey?
-    @in_use == true
+  def station #method of station
   end
 
-  def station
-  end
+  private #private method provides functionality for the public methods the user wants to see (but is not needed for user)
 
-  private
-
-  def deduct(money)
-    @balance -= money
+  def deduct(money) #as deduct provides functionality for touch_out it is no longer needed in the public method
+    @balance -= money # same as balance = balance - money (therefore deducting it)
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,82 +6,82 @@ describe Oystercard do
   let(:station) { Station.new }
 
   it 'responds to balance' do
-    expect(subject).to respond_to(:balance)
+    expect(subject).to respond_to(:balance) #expects oystercard to respond to balance
   end
 
   it 'initializes with a balance of zero' do
-    expect(subject.balance).to eq 0
+    expect(subject.balance).to eq 0 #expect oystercard balance to equal zero
   end
 
   it 'responds to top_up' do
-    expect(subject).to respond_to(:top_up).with(1).argument
+    expect(subject).to respond_to(:top_up).with(1).argument #expect oystercard to respond to top up(with 1 argument)
   end
 
   it 'oystercard responds to touch_in' do
-    expect(subject).to respond_to(:touch_in).with(1).argument
+    expect(subject).to respond_to(:touch_in).with(1).argument #expect oystercard to respond to touch in (with 1 argument)
   end
 
   it 'responds to touch_out' do
-    expect(subject).to respond_to(:touch_out)
+    expect(subject).to respond_to(:touch_out) # expect oystercard to respond to touch out 
   end
 
   it 'responds to in_journey?' do
-    expect(subject).to respond_to(:in_journey?)
+    expect(subject).to respond_to(:in_journey?) # expect oystercard to respond to in journey? 
   end
 
   describe '#top_up' do
     it 'will top up the balance' do
-      expect { subject.top_up(1) }.to change { subject.balance }.by 1
+      expect { subject.top_up(1) }.to change { subject.balance }.by 1 #expect oystercard.top up to change the balance by 1
     end
 
     it 'raises error if user tries to topup past max capacity' do
-      subject.capacity.times { subject.top_up(1) }
+      subject.capacity.times { subject.top_up(1) } # oystercard.capacity 
       expect { subject.top_up(1) }.to raise_error "Cap  of #{subject.capacity} reached"
     end
   end
 
   describe '#touch_in' do
     it "changes @in_use from false to true" do
-      subject.top_up(subject.minimum_fare)
-      subject.touch_in(station)
-      expect(subject.in_use).to eq true
+      subject.top_up(subject.minimum_fare) # ystercard.top_up (by the minimum fare ) in order to make test passo
+      subject.touch_in(station) # oystercard.touch_in(to station)
+      expect(subject.in_use).to eq true # expect oystercard to be in use
     end
 
     it 'touch in fails when balance < 1' do
-      expect { subject.touch_in(station) }.to raise_error "Not enough funds"
+      expect { subject.touch_in(station) }.to raise_error "Not enough funds" # expect subject.touch_in to fail due to there being not enough funds
     end
 
     it 'remembers the station' do
-      subject.top_up(subject.minimum_fare)
-      subject.touch_in(station)
-      expect(subject.start_station).to eq station 
+      subject.top_up(subject.minimum_fare) # subject.top_up (by minimum fare) in order to make test work
+      subject.touch_in(station) # oystercard.touch_in(to station)
+      expect(subject.start_station).to eq station  # expect subject.start_station to equal station
     end
   end
 
   describe '#touch_out' do
     it ' changes @in_use from true to false' do
-        subject.top_up(subject.minimum_fare)
-        subject.touch_in(station)
-        subject.touch_out
-      expect(subject.in_use).to eq false
+        subject.top_up(subject.minimum_fare) #oystercard.top_up (by the minimum fare ) in order to make test pass
+        subject.touch_in(station) #oystercard.touch_in(to station) to make test pass
+        subject.touch_out #oystercard.touch_out of station to make test pass
+      expect(subject.in_use).to eq false # expect oystercard to not be in use
     end
 
     it 'deducts minimum fare from balance' do
-      subject.top_up(subject.minimum_fare)
-      subject.touch_in(station)
-      expect { subject.touch_out}. to change{ subject.balance }.by(-(subject.minimum_fare))
+      subject.top_up(subject.minimum_fare) #oystercard.top_up (by the minimum fare ) in order to make test pass
+      subject.touch_in(station) #oystercard.touch_in(to station) to make test pass
+      expect { subject.touch_out}. to change{ subject.balance }.by(-(subject.minimum_fare)) #expect subject.touch_out to change the balance by (minus) the minimum fare
     end
   end
 
   describe '#in_journey?' do
     it 'return true if card is in use' do
-      subject.top_up(subject.minimum_fare)
-      subject.touch_in(station)
-      expect(subject).to be_in_journey
+      subject.top_up(subject.minimum_fare) #oystercard.top_up (by the minimum fare ) in order to make test pass
+      subject.touch_in(station)  #oystercard.touch_in(to station) to make test pass
+      expect(subject).to be_in_journey # expect oystercard(owner) to be in journey after you've touched in
     end
 
     it 'returns false if card not in use' do
-      expect(subject).not_to be_in_journey
+      expect(subject).not_to be_in_journey # expect subject to not be in journey
     end
   end
 end
