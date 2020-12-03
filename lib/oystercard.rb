c59bf8fc8077e9fd39dd0d7ee979257604041c12
Lisa <bardelli.lisa@gmail.com>
2020-12-03 17:03:21 +0000

    Add journey history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,43 +1,56 @@
-require "oystercard/version"
-
+require './lib/oystercard'
+# require "oystercard/version"
 module Oystercard
   class Card
     MAX_BALANCE = 100
     MIN_FARE = 1
-    attr_reader :balance, :entry_station
+    attr_reader :balance, :entry_station, :exit_station
+    attr_accessor :journeys
 
     def initialize
       @balance = 0
       @entry_station = entry_station
+      @exit_station = exit_station
+      @journeys = []
+      @journey = { in: nil, out: nil }
     end
 
-#this method has been tested
+    # this method has been tested
     def top_up(money)
-      fail "Exceeds max balance" if balance + money >= MAX_BALANCE
-      @balance = @balance + money
+      raise 'Exceeds max balance' if balance + money >= MAX_BALANCE
+
+      @balance += money
     end
 
-#this method has been tested
+    # this method has been tested
     def touch_in(entry_station)
-      fail "Insufficient funds" if balance < MIN_FARE
+      raise 'Insufficient funds' if balance < MIN_FARE
+
       @entry_station = entry_station
     end
 
-#this method has been tested
+    # this method has been tested
     def in_journey?
       !!@entry_station
     end
 
-#this method has been tested
-    def touch_out
+    # this method has been tested
+    def touch_out(exit_station)
       deduct(MIN_FARE)
-      @entry_station = nil
+      # @entry_station = nil --> returns nil for entry station
+      @exit_station = exit_station
+    end
+
+    def store_journey
+      @journey[:in] = @entry_station
+      @journey[:out] = @exit_station
+      @journey
     end
 
     private
 
     def deduct(money)
-      @balance = @balance - money
+      @balance -= money
     end
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,20 +1,19 @@
 RSpec.describe Oystercard::Card do
-
-#let(:oystercard) {Oystercard.new}
-let(:entry_station) {double :entry_station}
-
-  describe "#top_up" do
-    it "tops up the balance" do
+  # let(:oystercard) {Oystercard.new}
+  let(:entry_station) { double :entry_station }
+  let(:exit_station) { double :exit_station }
+  describe '#top_up' do
+    it 'tops up the balance' do
       subject.top_up(10)
       expect(subject.balance).to eq(10)
     end
 
-    it "raises an error when reaches max balance" do
-      expect { subject.top_up(101) }.to raise_error "Exceeds max balance"
+    it 'raises an error when reaches max balance' do
+      expect { subject.top_up(101) }.to raise_error 'Exceeds max balance'
     end
   end
 
-  describe "#touch_in" do
+  describe '#touch_in' do
     # it "touch in to start a journey" do
     #   subject.top_up(10)
     #   #expect(subject).not_to be_in_journey
@@ -22,25 +21,25 @@ let(:entry_station) {double :entry_station}
     #   #expect(subject).to be_in_journey
     # end
 
-    it "raises error when insufficient funds" do
+    it 'raises error when insufficient funds' do
       expect { subject.touch_in(entry_station) }.to raise_error 'Insufficient funds'
     end
 
-    it "saves the entry station" do
+    it 'saves the entry station' do
       subject.top_up(10)
       expect(subject.touch_in(entry_station)).to eq(entry_station)
     end
   end
 
-  describe "#in_journey" do
-    it "returns not nil when travelling" do
+  describe '#in_journey' do
+    it 'returns not nil when travelling' do
       subject.top_up(10)
       subject.touch_in(entry_station)
       expect(subject.in_journey?).not_to eq(nil)
     end
   end
 
-  describe "#touch_out" do
+  describe '#touch_out' do
     # it "touch out to end journey" do
     #   subject.top_up(10)
     #   subject.touch_in(entry_station)
@@ -48,15 +47,33 @@ let(:entry_station) {double :entry_station}
     #   expect(subject).not_to be_in_journey
     # end
 
-    it "reduces the balance by minimum fare" do
-      expect { subject.touch_out }.to change{ subject.balance }.by(-Oystercard::Card::MIN_FARE)
+    it 'reduces the balance by minimum fare' do
+      expect { subject.touch_out(exit_station) }.to change { subject.balance }.by(-Oystercard::Card::MIN_FARE)
     end
 
-    it "returns nil for entry station" do
+    # it "returns nil for entry station" do
+    #   subject.top_up(10)
+    #   subject.touch_in(entry_station)
+    #   subject.touch_out(exit_station)
+    #   expect(subject.entry_station).to eq(nil)
+    # end
+  end
+  describe '#store_journey' do
+    it 'returns an hash with nil values ' do
+      subject.top_up(10)
+      expected = { in: nil, out: nil }
+      expect(subject.store_journey).to eq(expected)
+    end
+
+    it 'returns an hash when storing_journey' do
       subject.top_up(10)
       subject.touch_in(entry_station)
-      subject.touch_out
-      expect(subject.entry_station).to eq(nil)
+      subject.touch_out(exit_station)
+      expected = { in: entry_station, out: exit_station }
+      expect(subject.store_journey).to eq(expected)
     end
+  end
+  it 'returns empty list for journeys if not travelling' do
+    expect(subject.journeys).to be_empty
   end
 end
