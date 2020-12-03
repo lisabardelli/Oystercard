665e408cf2f43cbcec75377d7090b3cbb65101f5
Irina <igiosanu29@gmail.com>
2020-12-03 15:56:44 +0000

    Saving the entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,30 +4,34 @@ module Oystercard
   class Card
     MAX_BALANCE = 100
     MIN_FARE = 1
-    attr_reader :balance, :in_use
+    attr_reader :balance, :entry_station
 
     def initialize
       @balance = 0
-      @in_use = false
+      @entry_station = entry_station
     end
 
+#this method has been tested
     def top_up(money)
       fail "Exceeds max balance" if balance + money >= MAX_BALANCE
       @balance = @balance + money
     end
 
-    def touch_in
+#this method has been tested
+    def touch_in(entry_station)
       fail "Insufficient funds" if balance < MIN_FARE
-      @in_use = true
+      @entry_station = entry_station
     end
 
+#this method has been tested
     def in_journey?
-      @in_use
+      !!@entry_station
     end
 
+#this method has been tested
     def touch_out
       deduct(MIN_FARE)
-      @in_use = false
+      @entry_station = nil
     end
 
     private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,12 @@
 RSpec.describe Oystercard::Card do
 
+#let(:oystercard) {Oystercard.new}
+let(:entry_station) {double :entry_station}
+
   describe "#top_up" do
     it "tops up the balance" do
       subject.top_up(10)
-      expect(subject.balance).to eql(10)
+      expect(subject.balance).to eq(10)
     end
 
     it "raises an error when reaches max balance" do
@@ -12,28 +15,48 @@ RSpec.describe Oystercard::Card do
   end
 
   describe "#touch_in" do
-    it "touch in to start a journey" do
-      subject.top_up(10)
-      expect(subject).not_to be_in_journey
-      subject.touch_in
-      expect(subject).to be_in_journey
-    end
+    # it "touch in to start a journey" do
+    #   subject.top_up(10)
+    #   #expect(subject).not_to be_in_journey
+    #   subject.touch_in(entry_station)
+    #   #expect(subject).to be_in_journey
+    # end
 
     it "raises error when insufficient funds" do
-      expect { subject.touch_in }.to raise_error 'Insufficient funds'
+      expect { subject.touch_in(entry_station) }.to raise_error 'Insufficient funds'
+    end
+
+    it "saves the entry station" do
+      subject.top_up(10)
+      expect(subject.touch_in(entry_station)).to eq(entry_station)
+    end
+  end
+
+  describe "#in_journey" do
+    it "returns not nil when travelling" do
+      subject.top_up(10)
+      subject.touch_in(entry_station)
+      expect(subject.in_journey?).not_to eq(nil)
     end
   end
 
   describe "#touch_out" do
-    it "touch out to end journey" do
-      subject.top_up(10)
-      subject.touch_in
-      subject.touch_out
-      expect(subject).not_to be_in_journey
-    end
+    # it "touch out to end journey" do
+    #   subject.top_up(10)
+    #   subject.touch_in(entry_station)
+    #   subject.touch_out
+    #   expect(subject).not_to be_in_journey
+    # end
 
     it "reduces the balance by minimum fare" do
       expect { subject.touch_out }.to change{ subject.balance }.by(-Oystercard::Card::MIN_FARE)
+    end
+
+    it "returns nil for entry station" do
+      subject.top_up(10)
+      subject.touch_in(entry_station)
+      subject.touch_out
+      expect(subject.entry_station).to eq(nil)
     end
   end
 end
